8f933631d749e940887d517470589aaedb03d64c
8harrison <monteiro4100@gmail.com>
2025-08-30 20:02:37 -0300

    refatoração de auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,25 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // carregar navbar dinâmica
   fetch("partials/navbar.html")
-    .then(res => res.text())
-    .then(html => {
+    .then((res) => res.text())
+    .then((html) => {
       document.getElementById("navbar-container").innerHTML = html;
 
       const user = localStorage.getItem("user");
       const navLinks = document.getElementById("nav-links");
+      const login = navLinks.querySelector('a[href="login.html"]');
+      const registro = navLinks.querySelector('a[href="registro.html"]');
+      const dashboard = navLinks.querySelector('a[href="dashboard.html"]');
+      const logout = navLinks.querySelector("#logoutBtn");
 
       if (user) {
         // mostra dashboard e sair
-        navLinks.querySelector('a[href="login.html"]').parentElement.classList.add("d-none");
-        navLinks.querySelector('a[href="registro.html"]').parentElement.classList.add("d-none");
-        navLinks.querySelector('a[href="dashboard.html"]').parentElement.classList.remove("d-none");
-        navLinks.querySelector('#logoutBtn').parentElement.classList.remove("d-none");
+        login.parentElement.classList.add("d-none");
+        registro.parentElement.classList.add("d-none");
+        dashboard.parentElement.classList.remove("d-none");
+        logout.parentElement.classList.remove("d-none");
       } else {
         // mostra login e registro
-        navLinks.querySelector('a[href="login.html"]').parentElement.classList.remove("d-none");
-        navLinks.querySelector('a[href="registro.html"]').parentElement.classList.remove("d-none");
-        navLinks.querySelector('a[href="dashboard.html"]').parentElement.classList.add("d-none");
-        navLinks.querySelector('#logoutBtn').parentElement.classList.add("d-none");
+        login.parentElement.classList.remove("d-none");
+        registro.parentElement.classList.remove("d-none");
+        dashboard.parentElement.classList.add("d-none");
+        logout.parentElement.classList.add("d-none");
 
         // protege dashboard
         if (window.location.pathname.endsWith("dashboard.html")) {
@@ -28,8 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
 
       // logout
-      document.getElementById("logoutBtn")?.addEventListener("click", () => {
-        localStorage.removeItem("authToken");
+      logout?.addEventListener("click", () => {
         localStorage.removeItem("user");
         window.location.href = "login.html";
       });
